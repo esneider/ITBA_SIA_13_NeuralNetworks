f4f12a6f6fe298ee786569d1293842ca04fe9306
esneider <dariosn@gmail.com>
2013-04-24 18:16:31 -0300

    doc

--- src/+algorithm/+debug/+plot/plotAll.m
+++ src/+algorithm/+debug/+plot/plotAll.m
@@ -5,10 +5,10 @@ function plotError(data)
     hold on;
 
     err = plot(data.alg.errors(10 : end),'r');
-    eta = plot(data.alg.etas(10 : end),'b');
+    eta = plot(data.alg.etas(10 : end) .* 50,'b');
     rb = plot(data.alg.rollbacks(10 : end),'g');
 
-    legend([err, eta, rb], {'mean sqared error', 'eta value', 'accumulated rollbacks / 1000'});
+    legend([err, eta, rb], {'mean sqared error', 'eta value * 50', 'accumulated rollbacks / 1000'});
 
     hold off;
 
