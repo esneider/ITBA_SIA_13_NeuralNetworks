b1d48c77634aac84d26f3a6d64bee96671189492
esneider <dariosn@gmail.com>
2013-04-24 12:50:25 -0300

    added input file

--- src/run.m
+++ src/run.m
@@ -1,4 +1,4 @@
-params = struct('runs', 5000, 'debug', '', 'path', 'output/');
+params = struct('runs', 5000, 'debug', '', 'path', 'output/', 'rollback', false, 'etaEps', 1);
 
 algorithm.main([-1 -1; -1 1; 1 -1; 1 1; 0 1; 0 -1; 1 0; -1 0], [0; 0; 0; 0; 1; 1; 1; 1], [4], params);
 % algorithm.main([-1 -1; -1 1; 1 -1; 1 1; 0 1; 0 -1; 1 0; -1 0], [0; 0; 0; 0; 1; 1; 1; 1], [2], params);
